c5946e7e96a8485a9ead764fa7301e1a5e7843ca
Artem Makhno <artem.makhno@dataart.com>
2020-01-30 13:33:24 +0300

    Restructure a bit

--- LogWriter/Helpers.cs
+++ LogWriter/Helpers.cs
@@ -1,21 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Globalization;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace LogWriter
 {
-    class Helpers
+    public class Helpers
     {
         internal static double[] GetWebPrices(string text, string[] ores)
         {
-            var regex = new Regex(@"^(?<price>\d+\.\d+) ISK");
+            var regex = new Regex(@"^(?<price>-?\d+(?: \d+)?(?:\.\d+)?) ISK");
             string[] htmlText = text.Split(new char[] { '\n' } );
             double[] Prices = new double[16];
             int position = 0;
@@ -32,21 +24,6 @@ namespace LogWriter
                         position = j + 4;
                         break;
                     }
-                    /*if (htmlText[j].Contains(','))
-                    {
-                        try
-                        {
-                            Prices[countOfPrices] = Convert.ToInt32(htmlText[j].Split(new char[] { ' ', '\r' }, StringSplitOptions.RemoveEmptyEntries)[1].Replace(",", null));
-                        }
-                        catch
-                        {
-                            Prices[countOfPrices] = Convert.ToInt32(htmlText[j].Substring(5).Replace(",", String.Empty).Replace("\r", String.Empty));
-                        }
-                        countOfPrices++;
-                        if (countOfPrices == 16)
-                            break;
-                        jOld = j;
-                    }             */       
                 }
                 if (countOfPrices == 16)
                     break;
